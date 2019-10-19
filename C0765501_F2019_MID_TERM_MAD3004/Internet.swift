d44b2626c493ea23a08716a63491957996226cf3
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-19 18:24:58 -0400

    initializer created

--- C0765501_F2019_MID_TERM_MAD3004/Internet.swift
+++ C0765501_F2019_MID_TERM_MAD3004/Internet.swift
@@ -12,6 +12,12 @@ class Internet: Bill
     var provider_name : String?
     var net_usage = Int()
     
-   
+    init(BillId: Int , BillDate: String , BillType: Float , provider_name: String , net_usage: Int)
+    {
+        super.init(BillId: <#T##Int#>, BillDate: <#T##String#>, Billtype: <#T##Bill.Types#>, Totalamount: <#T##Float#>)
+        self.provider_name = provider_name
+        self.net_usage = net_usage
+    }
     
+  
 }
