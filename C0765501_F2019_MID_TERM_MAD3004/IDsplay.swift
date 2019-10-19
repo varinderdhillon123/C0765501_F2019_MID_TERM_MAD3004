eee4dfe228b898ca371d9c736c423768ec5f0349
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-19 18:50:54 -0400

    protocol

---
+++ C0765501_F2019_MID_TERM_MAD3004/IDsplay.swift
@@ -0,0 +1,12 @@
+//
+//  IDsplay.swift
+//  C0765501_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+protocol IDisplay {
+    func display()
+}
