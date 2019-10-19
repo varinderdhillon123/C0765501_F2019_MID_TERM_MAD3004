6b069fe1e663e4316d47b0bcfcd94599c794b48f
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-19 18:58:38 -0400

    var 1

---
+++ C0765501_F2019_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  C0765501_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+var cus1 = (Cus_Id: 1 , Cus_fname:"Charanpreet" , Cus_lname:"Kaur" , Cus_email: "charanpreetkaur026@gmail.com")
+
+
+
