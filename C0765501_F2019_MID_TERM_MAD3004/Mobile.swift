0c8374717bd48b0ced0dcc8ac130d9cd8f10c599
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-19 18:38:21 -0400

    initializer declared

--- C0765501_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0765501_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -15,4 +15,16 @@ class Mobile: Bill
     var net_used: Int?
     var min_used: Int?
     
+    init(BillId: Int , BillDate: String , BillType: Float , mobile_manufacturer: String , plan_name: String , mobile_name: Int , net_used: Int , min_used: Int)
+    {
+        super.init(BillId: <#T##Int#>, BillDate: <#T##String#>, Billtype: <#T##Bill.Types#>, Totalamount: <#T##Float#>)
+        self.mobile_manufacturer = mobile_manufacturer
+        self.plan_name = plan_name
+        self.mobile_num = mobile_name
+        self.min_used = min_used
+        self.net_used = net_used
+    }
+    
+    
+    
 }
