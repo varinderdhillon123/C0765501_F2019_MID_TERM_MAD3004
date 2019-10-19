b5e519c58c081dc0c09d2e9abaa4e355a760d6e2
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-19 18:12:12 -0400

    class with variables

---
+++ C0765501_F2019_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,14 @@
+//
+//  Hydro.swift
+//  C0765501_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Hydro:Bill
+{
+    var agency_name: String?
+    var Units_Consumed: Int?
+}
