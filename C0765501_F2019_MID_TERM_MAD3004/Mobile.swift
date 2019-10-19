5ed3b40ee5fd1e9a9625473289698376cfa02a8f
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-19 19:11:24 -0400

    updated

--- C0765501_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0765501_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -15,16 +15,24 @@ class Mobile: Bill
     var net_used: Int?
     var min_used: Int?
     
-    init(BillId: Int , BillDate: String , BillType: Float , mobile_manufacturer: String , plan_name: String , mobile_name: Int , net_used: Int , min_used: Int)
+    init(BillId: Int , BillDate: String , BillType: Float , Totalamount: Float, mobile_manufacturer: String , plan_name: String , mobile_name: Int , net_used: Int , min_used: Int)
     {
         super.init(BillId: <#T##Int#>, BillDate: <#T##String#>, Billtype: <#T##Bill.Types#>, Totalamount: <#T##Float#>)
         self.mobile_manufacturer = mobile_manufacturer
         self.plan_name = plan_name
-        self.mobile_num = mobile_name
+        self.mobile_num = mobile_num
         self.min_used = min_used
         self.net_used = net_used
     }
     
     
-    
+    override func display()
+    {
+        super.display()
+        print("Manufacturer Name: \(mobile_manufacturer!)")
+        print(" Name of Plan: \(plan_name!)")
+        print("Mobile Number: \(mobile_num!)")
+        print("Internet Used: \(net_used!.GB())")
+        print("Minuted Used: \(min_used!.Minutes())")
+    }
 }
